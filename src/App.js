6a463a3e844057bb97a7975ea437566b4f994c9b
sourav6787 <84308623+sourav6787@users.noreply.github.com>
2023-02-15 21:45:35 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [timeToAchieve, setTimeToAchieve] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      title: title,
+      description: description,
+      dueDate: dueDate,
+      timeToAchieve: timeToAchieve,
+      timestamp: new Date().toLocaleString(),
+      status: "pending",
+    };
+    setTasks([...tasks, newTask]);
+    setTitle("");
+    setDescription("");
+    setDueDate("");
+    setTimeToAchieve("");
+  };
+
+  const handleDelete = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const handleStatusChange = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].status =
+      newTasks[index].status === "pending" ? "completed" : "pending";
+    setTasks(newTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>To Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Task Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Task Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <input
+          type="date"
+          placeholder="Due Date"
+          value={dueDate}
+          onChange={(e) => setDueDate(e.target.value)}
+          required
+        />
+        <input
+          type="time"
+          placeholder="Time to Achieve"
+          value={timeToAchieve}
+          onChange={(e) => setTimeToAchieve(e.target.value)}
+          required
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className={task.status}>
+            <div>
+              <h3>{task.title}</h3>
+              <p>{task.description}</p>
+              <p>
+                Due: {task.dueDate} at {task.timeToAchieve}
+              </p>
+              <p>Created: {task.timestamp}</p>
+            </div>
+            <div>
+              <button onClick={() => handleStatusChange(index)}>
+                {task.status === "pending" ? "Mark Completed" : "Mark Pending"}
+              </button>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
